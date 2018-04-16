a8a3c5f38c0389d1358b9ed26779567d42038424
pereztjacob <pereztjacob@gmail.com>
2018-04-16 10:55:46 -0700

    even better errors, getting close

---
+++ mongojs/lib/routes/florists.js
@@ -0,0 +1,45 @@
+const notFound = require('./not-found');
+const florist = require('../models/florist');
+
+const get = (req, res) => {
+    const id = req.paths[1];
+    id ? getOne(id, req, res) : getAll(req, res);
+};
+
+const getOne = (id, req, res) => {
+    florist.selectOne(id)
+        .then(one => {
+            res.send(one);
+        });
+};
+
+const getAll = (req, res) => {
+    florist.find().then(florists => {
+        res.send(florists);
+    });
+};
+
+const post = (req, res) => {
+    florist.insert(req.body).then(saved => {
+        res.send(saved);
+    });
+};
+
+const put = (req, res) => {
+    florist.update(req.body).then(updated => {
+        res.send(updated);
+    });
+};
+
+const del = (req, res) => {
+    florist.delete(req.paths[1])
+        .then(() => res.send({ removed: true }));
+};
+
+const methods = { get, post, put, delete: del };
+
+
+module.exports = (req, res) => {
+    const method = methods[req.method.toLowerCase()] || notFound;
+    method(req, res);
+};
